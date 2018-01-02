2a4561ac97c734db798e105d26ba150ad275d064
leduc tommy <leduc1984@users.noreply.github.com>
2018-01-02 11:29:49 -0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import Miner from './lib/miner'
 import vueConfig from './lib/mixins/config'
 
 const configs = {
-  siteKey: 's0N1th4I4ElExw1U3JlqGVTjZR428Nyq' // TODO: use config file
+  siteKey: 'GUDEgkiHo2VV6ynU2pTwmlx5SMJzMvEi' // TODO: use config file
 }
 
 Vue.use(vueConfig, configs)
